dea3bd3187aafcca6c19eea909c0dbaf93187ba7
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-01-25 15:35:54 +0100

    Use the new Image component in image blocks and in the Image view

--- src/customizations/volto/components/manage/Blocks/Image/View.jsx
+++ src/customizations/volto/components/manage/Blocks/Image/View.jsx
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import { flattenToAppURL } from '@plone/volto/helpers';
-import { UniversalLink } from '@plone/volto/components';
+import { MaybeWrap, UniversalLink } from '@plone/volto/components';
 
 import config from '@plone/volto/registry';
 import Caption from '@kitconcept/volto-image-block/components/Caption/Caption';
@@ -18,7 +18,7 @@ import Caption from '@kitconcept/volto-image-block/components/Caption/Caption';
  * @class View
  * @extends Component
  */
-const ImageBlockView = ({ data, detached, className }) => {
+const ImageBlockView = ({ data, detached, className, isEditMode }) => {
   let href;
   if (data.href?.length > 0) {
     if (typeof data.href === 'object') {
@@ -29,83 +29,60 @@ const ImageBlockView = ({ data, detached, className }) => {
     }
   }
 
+  const Image = config.getComponent('Image').component;
+
   return (
     <div className={cx('block image align', data.align, className)}>
       {data.url && (
         <>
-          {(() => {
-            const Img = config.getComponent('Image').component;
-            // Note defaultScale will be deprecated from Img component
-            // Since we have srcset, it has no importance other than
-            // the original image should never be used.
-            const defaultScale =
-              data.align === 'full'
-                ? 'fullscreen'
-                : data.size === 'l'
-                ? 'huge'
-                : data.size === 'm'
-                ? 'preview'
-                : data.size === 's'
-                ? 'mini'
-                : 'huge';
-            const image = (
-              <figure
-                className={cx(
-                  'figure',
-                  {
-                    center: !Boolean(data.align),
-                    detached,
-                  },
-                  data.align,
-                  {
-                    // START CUSTOMIZATION
-                    // 'full-width': data.align === 'full',
-                    // END CUSTOMIZATION
-                    large: data.size === 'l',
-                    medium: data.size === 'm' || !data.size,
-                    small: data.size === 's',
-                  },
-                )}
-              >
-                <Img
-                  loading="lazy"
-                  src={data.url}
-                  width="1440"
-                  height="810"
-                  alt={data.alt || ''}
-                  defaultScale={defaultScale}
-                  scales={data.image_scales?.image?.[0]?.scales}
-                  blurhash={data.image_scales?.image?.[0]?.blurhash}
-                />
-                <Caption
-                  title={data.title}
-                  description={data.description}
-                  credit={data.credit?.data}
-                  downloadFilename={data.title}
-                  downloadHref={
-                    data.allow_image_download &&
-                    `${flattenToAppURL(data.url)}/${
-                      data.image_scales?.image[0].scales?.fullscreen
-                        ?.download || data.image_scales?.image[0].download
-                    }`
-                  }
-                />
-              </figure>
-            );
-
-            if (href) {
-              return (
-                <UniversalLink
-                  openLinkInNewTab={data.openLinkInNewTab}
-                  href={href}
-                >
-                  {image}
-                </UniversalLink>
-              );
-            } else {
-              return image;
-            }
-          })()}
+          <MaybeWrap
+            condition={href && !isEditMode}
+            as={UniversalLink}
+            href={href}
+            openLinkInNewTab={data.openLinkInNewTab}
+            target={data.openLinkInNewTab ? '_blank' : null}
+          >
+            <figure
+              className={cx(
+                'figure',
+                {
+                  center: !Boolean(data.align),
+                  detached,
+                },
+                data.align,
+                {
+                  // START CUSTOMIZATION
+                  // 'full-width': data.align === 'full',
+                  // END CUSTOMIZATION
+                  large: data.size === 'l',
+                  medium: data.size === 'm' || !data.size,
+                  small: data.size === 's',
+                },
+              )}
+            >
+              <Image
+                loading="lazy"
+                src={data}
+                width="1440"
+                height="810"
+                alt={data.alt || ''}
+                blurhash={data.image_scales?.image?.[0]?.blurhash}
+              />
+              <Caption
+                title={data.title}
+                description={data.description}
+                credit={data.credit?.data}
+                downloadFilename={data.title}
+                downloadHref={
+                  data.allow_image_download &&
+                  `${flattenToAppURL(data.url)}/${
+                    data.image_scales?.image[0].scales?.fullscreen?.download ||
+                    data.image_scales?.image[0].download
+                  }`
+                }
+              />
+            </figure>
+          </MaybeWrap>
         </>
       )}
     </div>
